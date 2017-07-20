d26e70f6f2866973ab0b1d60cbaad43f9f213a29
Serkan Ertan <s.ertan@lmu.de>
2017-07-20 11:46:08 +0200

    CI was added

---
+++ src/index.test.js
@@ -0,0 +1,23 @@
+var chai = require('chai');
+var jsdom = require('jsdom');
+var fs = require('fs');
+
+describe('Our first test', function(){
+    it('should pass', function(){
+        chai.expect(true).to.equal(true);
+    });
+});
+
+
+
+describe('index.html', function () {
+    it('should say hello', function (done) {
+        var index = fs.readFileSync('./src/index.html', "utf-8");
+        jsdom.env(index, function (err, window) {
+            var h1 = window.document.getElementsByTagName('h1')[0];
+            chai.expect(h1.innerHTML).to.equal("Hello World!");
+            done();
+            window.close();
+        })
+    })
+})
